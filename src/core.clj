f950483cc89e722bbc392a61d679b71b09e35897
Pam Rucinque <pamela.rucinque@gmail.com>
2016-05-28 17:41:34 +1000

    clean code no matter what language

--- src/core.clj
+++ src/core.clj
@@ -2,20 +2,18 @@
   (:require
     [clojure.string :as str]))
 
-(defn validate-input-format
-  "Validates input format is ##:##"
+(defn valid-input-format
+  "Returns true if format is ##:##"
   [input]
-  (if (nil? (re-matches #"\d{2}:\d{2}" input))
-    {:input nil, :error "Wrong input! It should be hh:mm"}
-    {:input input, :error nil}))
+  (not (nil? (re-matches #"\d{2}:\d{2}" input))))
 
 (defn parse-time
   "Takes a string with format ##:## and returns a records with hours and minutes"
   [input]
-  (if (not (nil? (:error (validate-input-format input))))
-    (:error (validate-input-format input))
-    (map (fn [value]
-         (Integer. value)) (str/split input #":"))))
+  (if (not (valid-input-format input))
+    {:input nil :error "Wrong input! It should be hh:mm"}
+    {:input (map (fn [value]
+         (Integer. value)) (str/split input #":")) :error nil}))
 
 (defn -main
   [& args]
--- test/core_test.clj
+++ test/core_test.clj
@@ -4,8 +4,8 @@
 
 (deftest parse-time-tests
   (testing "it should parse hh:mm to a map with hours and minutes"
-    (is (= 12 (first (core/parse-time "12:35"))))
-    (is (= 35 (second (core/parse-time "12:35")))))
+    (is (= 12 (first (:input (core/parse-time "12:35")))))
+    (is (= 35 (second (:input (core/parse-time "12:35"))))))
 
   (testing "it should handle when the input is malformed"
     (is (= 1 1)))
@@ -13,14 +13,14 @@
   (testing "it should handle when the values are not valid"
     (is (= 1 1))))
 
-(deftest validate-input-format-tests
+(deftest valid-input-format-tests
   (testing "it should return true when format is ##:##"
-    (is (= {:input "12:35", :error nil} (core/validate-input-format "12:35"))))
+    (is (= true (core/valid-input-format "12:35"))))
 
   (testing "it should return false when format not ##:##"
-    (is (= {:input nil, :error "Wrong input! It should be hh:mm"} (core/validate-input-format "1:35")))
-    (is (= {:input nil, :error "Wrong input! It should be hh:mm"} (core/validate-input-format "12:5")))
-    (is (= {:input nil, :error "Wrong input! It should be hh:mm"} (core/validate-input-format "12445")))
-    (is (= {:input nil, :error "Wrong input! It should be hh:mm"} (core/validate-input-format "125")))
-    (is (= {:input nil, :error "Wrong input! It should be hh:mm"} (core/validate-input-format "AA:45")))
-    (is (= {:input nil, :error "Wrong input! It should be hh:mm"} (core/validate-input-format "12:425")))))
+    (is (= false (core/valid-input-format "1:35")))
+    (is (= false (core/valid-input-format "12:5")))
+    (is (= false (core/valid-input-format "12445")))
+    (is (= false (core/valid-input-format "125")))
+    (is (= false (core/valid-input-format "AA:45")))
+    (is (= false (core/valid-input-format "12:425")))))
