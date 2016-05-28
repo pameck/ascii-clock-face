47781f33828d004e6969bf5ed13c713f5ca419aa
Pam Rucinque <pamela.rucinque@gmail.com>
2016-05-28 20:49:34 +1000

    Clojure formatting, getting used to it

--- src/clock_formatter.clj
+++ src/clock_formatter.clj
@@ -4,27 +4,20 @@
 
 (defn spacer
   [times]
-  (apply str (repeat times " "))
-)
+  (apply str (repeat times " ")))
 
 (defn formatDbl
   [init final]
-  (str (spacer init) 0 (spacer (- final init 1)) 0
-  )
-)
+  (str (spacer init) 0 (spacer (- final init 1)) 0))
 
 (defn formatSngl
   [init]
-  (str (spacer init) 0
-  )
-)
+  (str (spacer init) 0))
 
 (defn format-it
   [input]
-  println (str "hh = " (first input) " mm = " (second input))
-)
+  println (str "hh = " (first input) " mm = " (second input)))
 
 (defn getLines
   [input]
-  [(formatSngl 3) (formatDbl 2 4) " 0   0" "0     0" " 0   0" "  0 0" "   0"]
-)
+  [(formatSngl 3) (formatDbl 2 4) " 0   0" "0     0" " 0   0" "  0 0" "   0"])
--- test/clock_formatter_test.clj
+++ test/clock_formatter_test.clj
@@ -4,36 +4,18 @@
 
 (deftest format-it-tests
   (testing "it should format a clock with the correct hours"
-    (is (= "hh = 5 mm = 30" (clock-formatter/format-it [5 30]) )
-    )
-  )
-)
+    (is (= "hh = 5 mm = 30" (clock-formatter/format-it [5 30])))))
 
 (deftest getLines-tests
   (testing "it should get an array with 7 empty lines"
-    (is (= ["   0" "  0 0" " 0   0" "0     0" " 0   0" "  0 0" "   0"] (clock-formatter/getLines [0 0]) )
-    )
-  )
-
-  ; (testing "it should get an array with the correct ours"
-  ;   (is (= ["   0   " "  0 0  " " 0   0" "0     0" " 0   0" "  h 0" "   0"] (clock-formatter/getLines [7 0]) )
-  ;   )
-  ; )
-)
+    (is (= ["   0" "  0 0" " 0   0" "0     0" " 0   0" "  0 0" "   0"] (clock-formatter/getLines [0 0])))))
 
 
 (deftest spacer-tests
   (testing "it should format the number of spaces correctly"
-    (is (= "   " (clock-formatter/spacer 3) )
-    )
-    (is (= "" (clock-formatter/spacer 0) )
-    )
-  )
-)
+    (is (= "   " (clock-formatter/spacer 3)))
+    (is (= "" (clock-formatter/spacer 0)))))
 
 (deftest formatSngl-tests
   (testing "it should format the number with the correct format"
-    (is (= "   0" (clock-formatter/formatSngl 3) )
-    )
-  )
-)
+    (is (= "   0" (clock-formatter/formatSngl 3)))))
