b0bd1561b8c8a387472d5781ad32bbe0ef2e7c6a
Pam Rucinque <pamela.rucinque@gmail.com>
2016-05-29 19:49:31 +1000

    two args instead of array

--- test/clock_formatter_test.clj
+++ test/clock_formatter_test.clj
@@ -1,6 +1,6 @@
 (ns clock-formatter-test
   (:require [clojure.test :refer :all])
-  (:use [clock-formatter :only [format-it empty-clock point-minutes-hand point-hours-hand to-analog-format]]))
+  (:use [clock-formatter :only [format-it empty-clock point-minutes-hand point-hours-hand toanalog-fomat]]))
 
 (deftest format-it-tests
   (testing "it should return a clock-matrix representing a clockface with the time set"
@@ -8,12 +8,12 @@
 
 (deftest to-analog-format-tests
   (testing "it should transform hours to 1 to 12 format"
-    (is (= [0 32]  (to-analog-format [0 32])))
-    (is (= [1 32]  (to-analog-format [1 32])))
-    (is (= [2 32]  (to-analog-format [2 32])))
-    (is (= [0 32]  (to-analog-format [12 32])))
-    (is (= [1 32]  (to-analog-format [13 32])))
-    (is (= [11 32] (to-analog-format [23 32]))))
+    (is (= [0 32]  (to-analog-format 0 32)))
+    (is (= [1 32]  (to-analog-format 1 32)))
+    (is (= [2 32]  (to-analog-format 2 32)))
+    (is (= [0 32]  (to-analog-format 12 32)))
+    (is (= [1 32]  (to-analog-format 13 32)))
+    (is (= [11 32] (to-analog-format 23 32))))
 
   (testing "it should round down the minutes past the hour to the nearest 5"
     (is (= 1 1))
