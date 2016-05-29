e3e4fd8f3e1dadfea33f038996a2f3ae00c0aad5
Pam Rucinque <pamela.rucinque@gmail.com>
2016-05-29 17:16:49 +1000

    Pipe&Pam: Removing old approach functions

--- src/clock_formatter.clj
+++ src/clock_formatter.clj
@@ -2,25 +2,9 @@
   (:require
     [clojure.string :as str]))
 
-(defn spacer
-  [times]
-  (apply str (repeat times " ")))
-
-(defn formatDbl
-  [init final]
-  (str (spacer init) 0 (spacer (- final init 1)) 0))
-
-(defn formatSngl
-  [init]
-  (str (spacer init) 0))
-
 (defn format-it
   [input]
   println (str "hh = " (first input) " mm = " (second input)))
-
-(defn getLines
-  [input]
-  [(formatSngl 3) (formatDbl 2 4) " 0   0" "0     0" " 0   0" "  0 0" "   0"])
 
 (def emptyClock
   (ref (vector
