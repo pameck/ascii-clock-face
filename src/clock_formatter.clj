7493fb92d8868925968bbf4ae45fc9ee4d4428b9
afcastano <andresfelipecastano@gmail.com>
2016-05-29 14:11:31 +1000

    Pam-pipe: emtpy clock spike

--- src/clock_formatter.clj
+++ src/clock_formatter.clj
@@ -21,3 +21,32 @@
 (defn getLines
   [input]
   [(formatSngl 3) (formatDbl 2 4) " 0   0" "0     0" " 0   0" "  0 0" "   0"])
+
+(def emptyClock
+  (ref (vector
+    (vector "_" "_" "_" "0" "_" "_" "_")
+    (vector "_" "_" "0" "_" "0" "_" "_")
+    (vector "_" "0" "_" "_" "_" "0" "_")
+    (vector "0" "_" "_" "_" "_" "_" "0")
+    (vector "_" "0" "_" "_" "_" "0" "_")
+    (vector "_" "_" "0" "_" "0" "_" "_")
+    (vector "_" "_" "_" "0" "_" "_" "_")
+  ))
+)
+
+(defn setLabel
+  [clock value label]
+  (case value
+      1 (replaceInMatrix clock 1 4 "h")
+  )
+)
+
+(defn replaceInMatrix
+  [matrix row column label]
+  (replaceInVector matrix row (replaceInVector (nth matrix row) column label)) 
+)
+
+(defn replaceInVector 
+  [vct position newValue]
+  (assoc vct position newValue)
+)
