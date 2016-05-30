18d1513a5ce6d46cf821f8e5ef7cc01e00718e46
afcastano <andresfelipecastano@gmail.com>
2016-05-30 22:30:41 +1000

    Pam-Pipe: Prompt for input in go.sh

--- go.sh
+++ go.sh
@@ -1,3 +1,12 @@
 #!/bin/bash
+command -v lein >/dev/null 2>&1 || { echo "I require lein but it's not installed.  You can download it from: http://leiningen.org/#install" >&2; exit 1; }
 
-lein run "$1"
+if [ -z "$1" ]
+  then
+    echo "What time do you want me to print?"
+    read clockTime 
+  else
+    clockTime="$1"
+fi
+
+lein run "$clockTime"
